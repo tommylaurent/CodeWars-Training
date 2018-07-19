3140f35e48037c3e63450f99aa334fb870439cb9
Tommy Laurent <tommylaurent@protonmail.com>
2018-07-19 12:20:10 +0200

    revolve kata 7kyu_alphabet_symmetry

--- 7kyu_alphabet_symmetry.js
+++ 7kyu_alphabet_symmetry.js
@@ -1,28 +1,21 @@
-/************************************************ 
-ALPHABET SYMMETRY - KYU 7 
-***********************************************/
+function solve(words) {
+  const alpha = "abcdefghijklmnopqrstuvwxyz";
+  const arrayAlpha = [...alpha];
+  const arrayAlphaM = arrayAlpha.map(x => x.toUpperCase());
+  const arrayWord = words.map(elt => [...elt]);
+  const indexAlpha = arrayAlpha.map(elt => elt + arrayAlpha.indexOf(elt));
+  const indexAlphaM = arrayAlphaM.map(elt => elt + arrayAlphaM.indexOf(elt));
+  const indexWord = arrayWord.map(elt => elt.map((x, index) => x + index));
+  const intersection = indexWord.map(elt =>
+    elt.filter(x => indexAlpha.includes(x))
+  );
+  const numberMin = intersection.map(x => x.length);
+  const intersectionM = indexWord.map(elt =>
+    elt.filter(x => indexAlphaM.includes(x))
+  );
+  const numberMaj = intersectionM.map(x => x.length);
+  return numberMin.map(x => x + numberMaj.shift());
+}
 
-/**********************************************
-Consider the word "abode".We can see that the letter a is in position 1 and b is in position 2. In the alphabet, a and b are also in positions 1 and 2. Notice also that d and e in abode occupy the positions they would occupy in the alphabet, which are positions 4 and 5.
-
-Given an array of words, return an array of the number of letters that occupy their positions in the alphabet for each word.For example, solve(["abode", "ABc", "xyzD"]) = [4, 3, 1].See test cases for more examples.
-
-Input will consist of alphabet characters, both uppercase and lowercase.No spaces.
-
-Good luck!
-
-https://www.codewars.com/kata/alphabet-symmetry/train/javascript
-
----
-
-  Comparer l'emplacement des lettre du mot par rapport a l'emplacement des lettre dans l'alphabet:
-
-"abode" = 4
-"abcde"
-
-ici 4 lettres ce place au même endroit dans l'alphabet et dans le mot (a=1, b=2, d=4, e=5)
-
-"xyzD" = 1
-abcD
-ici 1 lettre ce place au même endroit dans l'alphabet et dans le mot (D=4)
-***********************************************/
+const test = solve(["iamdefandjkl", "thedefgh", "xyzDEFghijabc"]);
+console.log(test);
