5bce7b35855664f71d86831ef1ac881cf6eebc00
Tommy Laurent <tommylaurent@protonmail.com>
2018-07-24 23:13:29 +0200

    add correction 6kyu_Count_the_smiley_faces

--- 6kyu_Count_the_smiley_faces.js
+++ 6kyu_Count_the_smiley_faces.js
@@ -2,7 +2,7 @@
 //* 6kyu_Count_the_smiley_faces
 /******************************************************************************************/
 
-//! utiliser .match pour comparer sa regex à un élément
+//! utiliser .match ou .test pour comparer sa regex à un élément
 
 /******************************************************************************************/
 //* EXERCICE
@@ -15,3 +15,22 @@ function countSmileys(arr) {
 
 const test = countSmileys([":)", ":(", ":D", ":O", ":;"]);
 console.log(test);
+
+/******************************************************************************************/
+//* CORRECTION 1
+/******************************************************************************************/
+
+//todo: il est possible de raccourcir encore plus en mettant la regex dans le filter
+
+// function countSmileys(arr) {
+//    return arr.filter(x => /^[:;][-~]?[)D]$/.test(x)).length;
+// }
+
+/******************************************************************************************/
+//* CORRECTION 2
+/******************************************************************************************/
+
+//todo: a voir le reduce
+
+// const countSmileys = ss =>
+//   ss.reduce((a, s) => a + /^[:;][-~]?[D)]$/.test(s), 0);
