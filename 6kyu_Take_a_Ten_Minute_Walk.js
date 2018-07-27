9422fabda33112d3a61213382758cbe9f03c325d
Tommy Laurent <tommylaurent@protonmail.com>
2018-07-27 23:59:50 +0200

    6kyu_Take_a_Ten_Minute_Walk regex and tab

--- 6kyu_Take_a_Ten_Minute_Walk.js
+++ 6kyu_Take_a_Ten_Minute_Walk.js
@@ -2,9 +2,27 @@
 //* 6kyu_Take_a_Ten_Minute_Walk
 /******************************************************************************************/
 
+// n.length === s.length && w.length === e.length;
+// walk.lenght === 10
+
 function isValidWalk(walk) {
-  console.log([...walk]);
+  const cardinal = ["n", "s", "w", "e"];
+  let resultat = [];
+  cardinal.map(e => {
+    const regex = new RegExp(e, "g");
+    resultat.push(walk.join("").match(regex));
+  });
+  // const walkDirection = walk.sort((a, b) => a > b);
+  // const ArrayDirection = walkDirection.reduce(
+  //   (s, v, i) => s + (v.charCodeAt() == i + 97),
+  //   0
+  // );
+  return resultat ? ;
+  //.reduce((a, c) => a + c);
+  //   const walk
+  // return walkDirection;
+  //   walk.reduce((a, c) => a + c);
 }
 
-const test = ["n", "s", "n", "s", "n", "s", "n", "s", "n", "s"];
-console.log(test3);
+const test = isValidWalk(["n", "s", "n", "s", "n", "s", "n", "s", "n", "s"]);
+console.log(test);
