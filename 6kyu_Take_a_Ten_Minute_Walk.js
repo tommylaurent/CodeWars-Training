e8c397696ca1795789bb55e78dbfa0603122395e
Tommy Laurent <tommylaurent@protonmail.com>
2018-07-29 14:17:55 +0200

    result 6kyu_Take_a_Ten_Minute_Walk

--- 6kyu_Take_a_Ten_Minute_Walk.js
+++ 6kyu_Take_a_Ten_Minute_Walk.js
@@ -2,30 +2,54 @@
 //* 6kyu_Take_a_Ten_Minute_Walk
 /******************************************************************************************/
 
-// n.length === s.length && w.length === e.length;
-// walk.lenght === 10
+// function isValidWalk(walk) {
+//   const cardinal = ["n", "s", "w", "e"];
+//   let resultat = [];
+//   cardinal.map(e => {
+//     const regex = new RegExp(e, "g");
+//     resultat.push(walk.join("").match(regex));
+//   });
+//   const nordSud = resultat.map(elt => elt.filter(e => e === "n" || e === "s"));
+//   const estWest = resultat.map(elt => elt.filter(e => e === "w" || e === "e"));
+//   const newNordSud = nordSud.filter(e => e.length);
+//   const newEstWest = estWest.filter(e => e.length);
+//   const direction = [newNordSud, newEstWest];
+//   const resultatDirection = direction.map(elt => elt.map(e => e.length));
+//   const minuteDirection = resultatDirection.map(
+//     elt => (elt.reduce((a, c) => a - c) === 0 ? elt : [999])
+//   );
+//   const minute = minuteDirection.map(elt => elt.reduce((a, c) => a + c));
+//   return minute.reduce((a, c) => a + c) === 10 ? true : false;
+// }
+
+// const test = isValidWalk(["n", "s", "n", "s", "n", "s", "e", "e", "w", "w"]);
+// console.log(test);
 
 function isValidWalk(walk) {
-  const cardinal = ["n", "s", "w", "e"];
-  let resultat = [];
-  cardinal.map(e => {
+  const cardinal1 = ["n", "s"];
+  let newNordSud = [];
+  cardinal1.map(e => {
     const regex = new RegExp(e, "g");
-    resultat.push(walk.join("").match(regex));
+    newNordSud.push(walk.join("").match(regex));
   });
-  // const walkDirection = walk.sort((a, b) => a > b);
-  // const ArrayDirection = walkDirection.reduce(
-  //   (s, v, i) => s + (v.charCodeAt() == i + 97),
-  //   0
-  // );
-  resultatFilter = resultat.filter(elt => elt !== null);
-
-  return resultatFilter.map(elt => elt.length);
-
-  //.reduce((a, c) => a + c);
-  //   const walk
-  // return walkDirection;
-  //   walk.reduce((a, c) => a + c);
+  const cardinal2 = ["w", "e"];
+  let newEstWest = [];
+  cardinal2.map(e => {
+    const regex = new RegExp(e, "g");
+    newEstWest.push(walk.join("").match(regex));
+  });
+  const direction = [newNordSud, newEstWest];
+  console.log(direction);
+  const resultatDirection = direction.map(elt =>
+    elt.map(elt => (elt !== null ? elt.length : 0))
+  );
+  console.log(resultatDirection);
+  const minuteDirection = resultatDirection.map(
+    elt => (elt.reduce((a, c) => a - c) === 0 ? elt : [999])
+  );
+  const minute = minuteDirection.map(elt => elt.reduce((a, c) => a + c));
+  return minute.reduce((a, c) => a + c) === 10 ? true : false;
 }
 
-const test = isValidWalk(["n", "s", "n", "s", "n", "s", "n", "s", "n", "s"]);
+const test = isValidWalk(["n", "s", "n", "s", "n", "s", "e", "e", "w", "w"]);
 console.log(test);
